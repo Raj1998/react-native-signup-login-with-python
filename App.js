5c8ab58b729fbadecd893b805890c894a6ab1f3e
Raj <raj1998211@gmail.com>
2019-04-24 11:27:42 +0530

    start up redirect done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { 
           createSwitchNavigator,
           createAppContainer,
@@ -21,24 +21,8 @@ const AppSwitchNavigator = createSwitchNavigator({
 const AppContainer = createAppContainer(AppSwitchNavigator);
 class App extends React.Component {
 
-  _isSignedIn = async () => {
-    // try {
-      const value = await AsyncStorage.getItem('USER');
-      if (value !== null) {
-        // We have data!!
-        console.log('data che....'+value);
-        // this.props.navigation.navigate('Dashboard');
-      }
-    // } catch (error) {
-    //   // Error retrieving data
-    //   console.log('exeptn aai -----');
-    // }
-  }
 
-  constructor(){
-    super();
-    this._isSignedIn();
-  }
+
 
   render() {
     return (
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, AsyncStorage } from 'react-native';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 import { 
@@ -11,6 +11,25 @@ import {
 
 
 class WelcomeScreen extends Component {
+  _isSignedIn = async () => {
+    // try {
+      const value = await AsyncStorage.getItem('USER');
+      if (value !== null) {
+        // We have data!!
+        console.log('data che....'+value);
+        this.props.navigation.navigate('Dashboard');
+      }
+    // } catch (error) {
+    //   // Error retrieving data
+    //   console.log('exeptn aai -----');
+    // }
+  }
+
+  constructor(){
+    super();
+    this._isSignedIn();
+  }
+
   static navigationOptions = ({ navigation }) => {
     return{
       title: "Welcome !!!",
